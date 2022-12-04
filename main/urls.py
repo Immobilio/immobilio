c7aa36a88c36f9fa1e0fb135737ce9ed6a68e8ea
azer <azer.charaa@digitalberry.fr>
2022-12-04 01:22:31 +0100

    Remove the urls used for test

--- main/urls.py
+++ main/urls.py
@@ -12,10 +12,4 @@ router.register('program', ProgramView, basename="program")
 router.register('immobilier', ImmobilierView, basename="immobilier")
 
 
-urlpatterns = [
-    path("get_actif_programs", get_actif_programs, name="get_actif_programs"),
-    path("get_specific_price", get_specific_price, name="get_specific_price"),
-    path("get_piscine", get_piscine, name="get_piscine"),
-    path("promo_request", promo_request, name="promo_request"),
-    path("specific", specific, name="specific")
-] + router.urls
+urlpatterns = router.urls
